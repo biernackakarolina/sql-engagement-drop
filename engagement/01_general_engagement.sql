d1dbd31ef44bdd62bd0e39472b1d9c60d61e2440
Karolina Biernacka <biernacka.karolinaa@gmail.com>
2025-05-13 16:42:37 +0200

    Update and rename countries.sql to 01_general_engagement.sql

---
+++ engagement/01_general_engagement.sql
@@ -0,0 +1,13 @@
+-- selecting the number of engaged users in June - Septemper 2014
+SELECT drp.time_id,
+       COUNT(DISTINCT e.user_id) AS weekly_active_users
+  FROM benn.dimension_rollup_periods drp
+  LEFT JOIN tutorial.search_events e
+    ON e.occurred_at >= drp.pst_start
+   AND e.occurred_at < drp.pst_end
+   AND e.event_type = 'engagement'
+ WHERE drp.period_id = 1007
+   AND drp.time_id >= '2014-06-01'
+   AND drp.time_id < '2014-09-01'
+ GROUP BY 1
+ ORDER BY 1
