e819072876e24232bdae6350b4fa64aa36e6da84
Karolina Biernacka <biernacka.karolinaa@gmail.com>
2025-05-13 17:14:42 +0200

    Update 02_engagement_types.sql

--- engagement/02_engagement_types.sql
+++ engagement/02_engagement_types.sql
@@ -4,11 +4,11 @@ SELECT sub.date,
        SUM(CASE WHEN sub.event_name = 'login' THEN sub.users END) AS login,
        SUM(CASE WHEN sub.event_name = 'send_message' THEN sub.users END) AS message
 FROM
---count of distinct users per event type per week
+--subquery counting distinct users per event type per week
     (SELECT DATE_TRUNC('week', occurred_at) AS date,
             event_name,
             COUNT(DISTINCT user_id) AS users
-    FROM tutorial.yammer_events
+    FROM tutorial.search_events
     WHERE event_type = 'engagement'
     GROUP BY 1, 2) sub
     
