1bdc741134eee69cae25fbc1997422b16469c48a
Karolina Biernacka <biernacka.karolinaa@gmail.com>
2025-05-13 17:06:09 +0200

    Create 02_engagement_types.sql

---
+++ engagement/02_engagement_types.sql
@@ -0,0 +1,16 @@
+--count of distict users for engagement event types per week
+SELECT sub.date, 
+       SUM(CASE WHEN sub.event_name = 'home_page' THEN sub.users END) AS home_page,
+       SUM(CASE WHEN sub.event_name = 'login' THEN sub.users END) AS login,
+       SUM(CASE WHEN sub.event_name = 'send_message' THEN sub.users END) AS message
+FROM
+--count of distinct users per event type per week
+    (SELECT DATE_TRUNC('week', occurred_at) AS date,
+            event_name,
+            COUNT(DISTINCT user_id) AS users
+    FROM tutorial.yammer_events
+    WHERE event_type = 'engagement'
+    GROUP BY 1, 2) sub
+    
+GROUP BY 1
+ORDER BY 1
